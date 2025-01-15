5653b5e78b929e3bf84bfd3796dd87c903cf5673
samuelrolandumg <smartinezl2@miumg.edu.gt>
2025-01-14 19:36:40 -0600

    first commit12

--- Database/config.js
+++ Database/config.js
@@ -7,7 +7,7 @@ module.exports = {
   password: process.env.DB_PASSWORD || '123', // Contraseña de la base de datos
   database: process.env.DB_DATABASE || 'inventario', // Nombre de la base de datos
   port: process.env.DB_PORT || 1433, // Puerto de la base de datos
-  dialect: "mysql", // Dialecto (MySQL, PostgreSQL, etc.)
+  dialect: "mysql2", // Dialecto (MySQL, PostgreSQL, etc.)
   pool: {
     max: 5, // Máximo de conexiones en el pool
     min: 0, // Mínimo de conexiones en el pool
